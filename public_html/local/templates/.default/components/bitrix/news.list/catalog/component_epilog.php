9f6442aba362aaa3fe1ad1d35fa8b2de21ae770d
dayAlone <andrey.slider@gmail.com>
2016-05-11 17:18:12 +0700

    Title

--- public_html/local/templates/.default/components/bitrix/news.list/catalog/component_epilog.php
+++ public_html/local/templates/.default/components/bitrix/news.list/catalog/component_epilog.php
@@ -1,6 +1,5 @@
 <?
   global $APPLICATION;
   $s = end($arResult['SECTION']['PATH']);
-  $APPLICATION->SetTitle(str_replace("<br/>", " ", html_entity_decode($s['~NAME']));
-
+  $APPLICATION->SetTitle(str_replace("<br/>", " ", html_entity_decode($s['~NAME'])));
 ?>
