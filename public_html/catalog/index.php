8a487dd4918ea841861f73a2350e7e5d5f9af2f7
dayAlone <andrey.slider@gmail.com>
2016-08-20 18:40:11 +0700

    Download

--- public_html/catalog/index.php
+++ public_html/catalog/index.php
@@ -7,49 +7,53 @@ require($_SERVER['DOCUMENT_ROOT'].'/include/section.php');
 
 if (isset($_REQUEST['ELEMENT_CODE']) || (isset($_REQUEST['SECTION_CODE']) && !$_GLOBALS['currentSection'])):
     $APPLICATION->IncludeComponent("bitrix:news.detail", "catalog", Array(
-      "IBLOCK_ID"     => 2,
-      "ELEMENT_CODE"  => isset($_REQUEST['ELEMENT_CODE']) ? $_REQUEST['ELEMENT_CODE'] : $_REQUEST['SECTION_CODE'],
-      "CHECK_DATES"   => "N",
-      "IBLOCK_TYPE"   => "content_ru",
-      "SET_TITLE"     => "Y",
-      "CACHE_TYPE"    => "A",
-      "PROPERTY_CODE" => array("GALLERY", "FILES", 'TITLE'),
-    ));
+    "IBLOCK_ID"     => 2,
+    "ELEMENT_CODE"  => isset($_REQUEST['ELEMENT_CODE']) ? $_REQUEST['ELEMENT_CODE'] : $_REQUEST['SECTION_CODE'],
+    "CHECK_DATES"   => "N",
+    "IBLOCK_TYPE"   => "content_ru",
+    "SET_TITLE"     => "Y",
+    "CACHE_TYPE"    => "A",
+    "PROPERTY_CODE" => array("GALLERY", "FILES", 'TITLE'),
+));
 elseif (isset($_REQUEST['SECTION_CODE'])):
-  $APPLICATION->IncludeComponent("bitrix:news.list", "catalog",
-  	array(
-  		"IBLOCK_ID"            => 2,
-  		"NEWS_COUNT"           => "60",
-  		"SORT_BY1"             => "ACTIVE_FROM",
-  		"SORT_ORDER1"          => "DESC",
-  		"DETAIL_URL"           => "/catalog/#SECTION_CODE#/#ELEMENT_CODE#/",
-  		"CACHE_TYPE"           => "A",
-  		"SET_TITLE"            => "N",
-  		"SHOW_DESCRIPTION"     => "Y",
-  		"DISPLAY_BOTTOM_PAGER" => "Y",
+    $APPLICATION->IncludeComponent("bitrix:news.list", "catalog",
+    array(
+        "IBLOCK_ID"            => 2,
+        "NEWS_COUNT"           => "60",
+        "SORT_BY1"             => "ACTIVE_FROM",
+        "SORT_ORDER1"          => "DESC",
+        "DETAIL_URL"           => "/catalog/#SECTION_CODE#/#ELEMENT_CODE#/",
+        "CACHE_TYPE"           => "A",
+        "SET_TITLE"            => "N",
+        "SHOW_DESCRIPTION"     => "Y",
+        "DISPLAY_BOTTOM_PAGER" => "Y",
         "PROPERTY_CODE" => array("DIVIDER"),
-  		"PARENT_SECTION_CODE"  => $_REQUEST['SECTION_CODE'],
-  	)
-	);
+        "PARENT_SECTION_CODE"  => $_REQUEST['SECTION_CODE'],
+    )
+);
 else:
-  $APPLICATION->SetPageProperty('body_class', 'page--blank page--production');
-  $APPLICATION->SetTitle('Продукция');
-  $APPLICATION->SetPageProperty('footer_class', ' footer--fullwidth');
-  $APPLICATION->SetPageProperty('toolbar_class', ' toolbar--white');
+    $APPLICATION->SetPageProperty('body_class', 'page--blank page--production');
+    $APPLICATION->SetTitle('Продукция');
+    $APPLICATION->SetPageProperty('footer_class', ' footer--fullwidth');
+    $APPLICATION->SetPageProperty('toolbar_class', ' toolbar--white');
 
-  $APPLICATION->IncludeComponent("bitrix:news.list", "buttons",
-      array(
-        "IBLOCK_ID"      => "2",
-        "NEWS_COUNT"     => "40",
-        "SORT_BY1"       => "SECTION_ID",
-        "SORT_ORDER1"    => "ASC",
-        "CACHE_TYPE"     => "A",
-        "SET_TITLE"      => "N",
-        "CLASS"          => "buttons--margin buttons--production",
-        "PARENT_SECTION" => "null"
-      ),
-      false
+    $APPLICATION->IncludeComponent("bitrix:news.list", "buttons",
+        array(
+            "IBLOCK_ID"      => "2",
+            "NEWS_COUNT"     => "40",
+            "SORT_BY1"       => "SECTION_ID",
+            "SORT_ORDER1"    => "ASC",
+            "CACHE_TYPE"     => "A",
+            "SET_TITLE"      => "N",
+            "CLASS"          => "buttons--margin buttons--production",
+            "PARENT_SECTION" => "null"
+        ),
+        false
     );
+    ?>
+    <a href='<?=COption::GetOptionString("grain.customsettings", 'catalog')?>' class='download download--margin  download--white'><?=svg('pdf')?><span>Скачать каталог</span></a>
+    <?
+
 endif;
 require($_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php');
 ?>
--- public_html/local/templates/.default/components/bitrix/menu/menu/template.php
+++ public_html/local/templates/.default/components/bitrix/menu/menu/template.php
@@ -1,7 +1,7 @@
 <? $this->setFrameMode(true);?>
 <?if(count($arResult)>0):?>
 <nav class="nav <?=$arParams['CLASS']?>"><?=($arParams['FRAME'] ? '<div class="nav__frame">':'')?><?foreach ($arResult as $key=>$item):?><a href="<?=$item['LINK']?>" <?=$item['PARAMS']['MORE']?> class="nav__item <?=($item['SELECTED']?'nav__item--active':'')?>"><?=$item['TEXT']?></a><?endforeach;?>
-<?=($arParams['FRAME'] ? '</div>':'')?>
+<?=($arParams['FRAME'] ? '<a href="'.COption::GetOptionString("grain.customsettings", 'catalog').'" class=\'download download--orange\'>'.svg('pdf').'<span>Скачать каталог</span></a></div>':'')?>
 <?
 if($arParams['SHOW']=="Y"):
 	$APPLICATION->ShowViewContent('menu');
