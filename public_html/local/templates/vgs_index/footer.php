b749cd952a28b2e7ae38ad566b8214c89f5f488f
dayAlone <andrey.slider@gmail.com>
2016-11-13 15:50:52 +0700

    footer

--- public_html/local/templates/vgs_index/footer.php
+++ public_html/local/templates/vgs_index/footer.php
@@ -2,7 +2,7 @@
 <?
   $APPLICATION->IncludeComponent("bitrix:news.list", "buttons",
       array(
-        "IBLOCK_ID"      => "2",
+        "IBLOCK_ID"      => 11,
         "NEWS_COUNT"     => "40",
         "SORT_BY1"       => "SECTION_ID",
         "SORT_ORDER1"    => "ASC",
@@ -19,7 +19,7 @@
 <?
   $APPLICATION->IncludeComponent("bitrix:news.list", "certs",
       array(
-        "IBLOCK_ID"            => "1",
+        "IBLOCK_ID"            => "15",
         "NEWS_COUNT"           => "99",
         "SORT_BY1"             => "SORT",
         "SORT_ORDER1"          => "ASC",
